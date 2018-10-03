531926cd0f8403c573ccdc46a1d6a38a6b260537
Cameron Macdonald <cjmacdonald@mavs.coloradomesa.edu>
2018-10-02 18:06:01 -0600

    first commit

---
+++ url.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+    echo nl2br("Welcome to The Quintessential Gamer!");
+echo "<br><br>";
+
+echo ("COMING SOON!");
+
+echo "<br><br>";
+echo nl2br("Here you will find:\r\n-An about mee \r\n -My personal social medial \r\n -My Store \r\n -Stream Schedule");
+
+
+     ?>
+
+  </body>
+</html>
